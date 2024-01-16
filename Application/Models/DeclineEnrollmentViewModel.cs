4a12ff71a56efee544ff9d6e4b09866bbb0aa4fc
hdsmathew <chnmathew@gmail.com>
2024-01-16 13:26:02 +0400

    feat: add validation for message input when declining enrollments

--- Application/Models/DeclineEnrollmentViewModel.cs
+++ Application/Models/DeclineEnrollmentViewModel.cs
@@ -5,7 +5,9 @@ namespace Core.Application.Models
     public class DeclineEnrollmentViewModel
     {
         public int EnrollmentId { get; set; }
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Message can only contain characters.")]
         [StringLength(150, ErrorMessage = "Message should be maximum 150 characters long.")]
+        [Required(ErrorMessage = "Decline Reason is required.")]
         public string ReasonMessage { get; set; }
     }
 }
